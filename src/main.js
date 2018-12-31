0b01e9abd91599860e16891495c5d3953ebd21ad
Risto Tapani Raihala <ristotapani@gmail.com>
2018-12-31 17:07:28 -0200

    routes

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
+import router from './router/index.js'
 import './plugins/vuetify'
 import App from './App.vue'
+
+
+
+
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
