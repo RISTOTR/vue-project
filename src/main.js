a6c56021517b967d0315e3a80bbecad0bd072133
Risto Tapani Raihala <ristotapani@gmail.com>
2019-01-04 23:49:46 +0100

    home

--- src/main.js
+++ src/main.js
@@ -2,12 +2,12 @@ import Vue from 'vue'
 import router from './router/index.js'
 import './plugins/vuetify'
 import App from './App.vue'
-
-
-
+import Vcarousel from 'vuetify'
 
 
 Vue.config.productionTip = false
+
+Vue.component('Vcarousel', Vcarousel)
 
 new Vue({
   render: h => h(App),
