3d060939121bc678af7ec9e99ede8464825e5aed
Risto Tapani Raihala <ristotapani@gmail.com>
2019-01-08 18:12:34 +0200

    date filter

--- src/components/Meetup/Meetup.vue
+++ src/components/Meetup/Meetup.vue
@@ -6,13 +6,10 @@
                     <v-card-title>
                         <h2 class="primary--text">{{ meetup.title }}</h2>
                     </v-card-title>
-                     <v-img
-                                :src="meetup.imageUrl"
-                                height="400px"
-                                >
+                     <v-img :src="meetup.imageUrl" height="400px">
                     </v-img>
                     <v-card-text>
-                        <div class="info--text">{{ meetup.date }} -- {{ meetup.location }}/div>
+                        <div class="info--text">{{ meetup.date | date}} {{ meetup.location }}</div>
                         <div>{{ meetup.description }}</div>
                     </v-card-text>
                     <v-card-actions>
--- src/main.js
+++ src/main.js
@@ -3,11 +3,15 @@ import router from './router/index.js'
 import './plugins/vuetify'
 import App from './App.vue'
 import Vcarousel from 'vuetify'
-import Datepicker from 'vuejs-datepicker';
+import Datepicker from 'vuejs-datepicker'
 import { store } from './store'
+import DateFilter from './filters/date'
+
 
 
 Vue.config.productionTip = false
+
+Vue.filter('date', DateFilter)
 
 Vue.component('Vcarousel', Vcarousel, 'Datepicker', Datepicker)
 
